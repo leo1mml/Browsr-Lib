46df23ee6e4aec9b4adb429bc67dbbea8961a4de
Leonel Lima <mml_leo@hotmail.com>
2023-07-28 00:39:30 +0100

    Make source accessible

--- Sources/BrowsrLib/Network/BrowsrSource.swift
+++ Sources/BrowsrLib/Network/BrowsrSource.swift
@@ -1,6 +1,13 @@
 import Foundation
 
 public struct BrowsrSource {
+    public init(baseUrl: String, listingPath: String, searchPath: String, authToken: String) {
+        self.baseUrl = baseUrl
+        self.listingPath = listingPath
+        self.searchPath = searchPath
+        self.authToken = authToken
+    }
+    
     public let baseUrl: String
     public let listingPath: String
     public let searchPath: String
